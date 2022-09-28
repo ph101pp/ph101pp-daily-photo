6073a4b4caa6c410cc1447b0730ae4e1d8ac8e95
Philipp Adrian <hello@philippadrian.com>
2022-09-28 18:09:33 -0400

    2nd deployment to goerli

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -1,23 +1,23 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const mutableUri = "https://arweave.net/1a-HDKflsjG2_b3jAuMYTrQyB13BNarkz0WZdCEBj0E/";
+  const mutableUri = "https://ph101pp-daily-photo.vercel.app/api/proxy/";
   const immutableUri = "https://arweave.net/1a-HDKflsjG2_b3jAuMYTrQyB13BNarkz0WZdCEBj0E/";
   const treasury = "0x7E00FE5a6AAc417167A1a506550255CAfB5196a6";
   const vault = "0x1347aeA833D7a54456EAa76f45b66a9d91d0afb2";
 
-  const PDPTokenId = await ethers.getContractFactory("Ph101ppDailyPhotoTokenId");
-  const pdpTokenId = await PDPTokenId.deploy();
-  await pdpTokenId.deployed();
+  // const PDPTokenId = await ethers.getContractFactory("Ph101ppDailyPhotoTokenId");
+  // const pdpTokenId = await PDPTokenId.deploy();
+  // await pdpTokenId.deployed();
 
-  console.log(`deployed: Ph101ppDailyPhotoTokenId`, pdpTokenId.address);
+  // console.log(`deployed: Ph101ppDailyPhotoTokenId`, pdpTokenId.address);
 
   const PDP = await ethers.getContractFactory("Ph101ppDailyPhotos", {
     libraries: {
-      Ph101ppDailyPhotoTokenId: pdpTokenId.address,
+      Ph101ppDailyPhotoTokenId: "0x9AFb855a02A44766dD6859448ecce97493322F01" // pdpTokenId.address,
     },
   });
-  const pdp = await PDP.deploy(immutableUri, mutableUri, treasury, vault);
+  const pdp = await PDP.deploy(mutableUri, treasury, vault);
   await pdp.deployed();
   console.log(`deployed: Ph101ppDailyPhotos`, pdp.address);
 
--- contract/scripts/interact.ts
+++ contract/scripts/interact.ts
@@ -1,29 +1,31 @@
 import { ethers } from "hardhat";
 
 const tokenIdAddress = "0x9AFb855a02A44766dD6859448ecce97493322F01";
-const photosAddress = "0xA4Cc886811b8110307171C3a60C0406DC5f7392a";
-
-// async function main() {
-//   const pdp = await ethers.getContractAt("Ph101ppDailyPhotos", photosAddress);
-  
-//   const input = await pdp.getMintRangeInput(1000);
-
-//   const tx = await pdp.mintPhotos(...input, 10);
-
-//   const receipt = await tx.wait();
-
-//   console.log(receipt)
-// }
+const photosAddress1 = "0xA4Cc886811b8110307171C3a60C0406DC5f7392a";
+const photosAddress2 = "0x41A8608Fd85a7a83ad314Ed008c635b5C5981011";
 
 async function main() {
-  const pdp = await ethers.getContractAt("Ph101ppDailyPhotos", photosAddress);
+  const pdp = await ethers.getContractAt("Ph101ppDailyPhotos", photosAddress2);
   
-  const tx = await pdp.setDefaultRoyalty("0x2852578456552053183201a7c99EF7FA813b3622", 500);
+  const input = await pdp.getMintRangeInput(10);
+
+  // const tx = await pdp.setMutableURI("https://ph101pp-daily-photo.vercel.app/api/proxy/");
+  const tx = await pdp.mintPhotos(...input, 20);
 
   const receipt = await tx.wait();
 
   console.log(receipt)
 }
+
+// async function main() {
+//   const pdp = await ethers.getContractAt("Ph101ppDailyPhotos", photosAddress);
+  
+//   const tx = await pdp.setDefaultRoyalty("0x2852578456552053183201a7c99EF7FA813b3622", 500);
+
+//   const receipt = await tx.wait();
+
+//   console.log(receipt)
+// }
 
 main()
   .then(() => process.exit(0))
