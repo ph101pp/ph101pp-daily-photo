9868d22298f1e85dc3303cf585a774c5b062b67c
Philipp Adrian <hello@philippadrian.com>
2022-10-16 14:06:17 -0300

    add login redirect

--- dapp/next.config.js
+++ dapp/next.config.js
@@ -1,4 +1,13 @@
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/login',
+        destination: '/api/auth/signin',
+        permanent: true,
+      },
+    ]
+  },
   env: {
     LATEST_MANIFEST_URI: process.env.LATEST_MANIFEST_URI
   }
