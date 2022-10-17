7b9a32e7a22b62d7c9072b1d00047e4c97535e1d
Philipp Adrian <hello@philippadrian.com>
2022-10-17 14:06:55 -0300

    Update deploy.ts

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -1,10 +1,10 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const mutableUri = "https://ph101pp-daily-photo.vercel.app/api/proxy/";
+  const mutableUri = "https://daily-photo.ph101pp.xyz/api/proxy/";
   const immutableUri = "https://arweave.net/1a-HDKflsjG2_b3jAuMYTrQyB13BNarkz0WZdCEBj0E/";
-  const treasury = "0x7E00FE5a6AAc417167A1a506550255CAfB5196a6";
-  const vault = "0x1347aeA833D7a54456EAa76f45b66a9d91d0afb2";
+  const treasury = "0x7E00FE5a6AAc417167A1a506550255CAfB5196a6"; // Decentraland
+  const vault = "0x1347aeA833D7a54456EAa76f45b66a9d91d0afb2"; //Jarvis
 
   const PDP = await ethers.getContractFactory("Ph101ppDailyPhotos");
   const pdp = await PDP.deploy(mutableUri, immutableUri, treasury, vault);
