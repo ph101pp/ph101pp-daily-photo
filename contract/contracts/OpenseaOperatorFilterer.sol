6f524a722e4669f0081a93057c14395be4645a0b
Philipp Adrian <hello@philippadrian.com>
2022-11-20 17:39:53 -0300

    add operator-filter-registry from npm and remove manual reference copy

--- contract/contracts/OpenseaOperatorFilterer.sol
+++ contract/contracts/OpenseaOperatorFilterer.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {IOperatorFilterRegistry} from "./reference/operator-filter-registry-main/src/IOperatorFilterRegistry.sol";
+import {IOperatorFilterRegistry} from "operator-filter-registry/src/IOperatorFilterRegistry.sol";
 
 /**
  * @title  OpenseaOperatorFilterer
