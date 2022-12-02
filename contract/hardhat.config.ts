f0cf2ed807fdc8ca1253648167512991c1fe9b32
Philipp Adrian <hello@philippadrian.com>
2022-12-02 01:38:58 -0300

    add size test

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-etherscan";
 import "hardhat-gas-reporter";
+import "hardhat-contract-sizer";
 import * as dotenv from 'dotenv';
 dotenv.config();
 
