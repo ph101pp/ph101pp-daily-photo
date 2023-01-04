21e8fa551b4e0e7672f5783cb07dab4d1a2a87a8
Philipp Adrian <hello@philippadrian.com>
2023-01-04 11:13:28 +0100

    deploy logs && interact setup for mainnet contract

--- contract/scripts/interact.ts
+++ contract/scripts/interact.ts
@@ -14,21 +14,24 @@ const photosAddress10 = "0x5433c0e7616848Ac8ba043dF4a8B4C62cF747171";
 const photosAddress11 = "0x9f2dD4Be989E5C806a3eEC34f8c26929fb6Cc4B0";
 const photosAddress12 = "0x4Ad20a44489a92dd36d89B4596c17C43efE88b57";
 const photosAddress13 = "0x2f084edfc16573117b8cbFC01e1206CfAe5589Ac";
+const photosAddress14 = "0x74F5ab6D014AEa53448B4CB8A735ae9B29b5AAe3";
 
+const mainnetAddress1 = "0x986A4010D6dF5754f66F72764487070615CBDE9A";
 async function main() {
-  const pdp = await ethers.getContractAt("Ph101ppDailyPhoto", photosAddress13);
+  const pdp = await ethers.getContractAt("Ph101ppDailyPhoto", photosAddress14);
   
   
   // do this first:
   // await pdp.setPermanentBaseUriUpTo("https://arweave.net/sc9G7tPVgyr9ajAl5erCclHUPq4N_i9hV_s8rjQ8n0o/", 0);
-  // await pdp.mintClaims("0x1347aeA833D7a54456EAa76f45b66a9d91d0afb2", 10, []);
+  // const tx = await pdp.mintClaims("0x1347aeA833D7a54456EAa76f45b66a9d91d0afb2", 10, []);
 
 
   // do this second:
-  const input = await pdp.getMintRangeInput(122);
-  const tx = await pdp.mintPhotos(...input);
+  // const input = await pdp.getMintRangeInput(122);
+  // const tx = await pdp.mintPhotos(...input);
+  
   const receipt = await tx.wait();
-  console.log(receipt)
+  console.log(receipt);
 
 
 }
