bf99b28cec813bba3be7f72917a8ae06da6fed4b
Philipp Adrian <hello@philippadrian.com>
2023-01-06 12:49:57 +0100

    Update getFutureMetadata.ts

--- dapp/utils/getFutureMetadata.ts
+++ dapp/utils/getFutureMetadata.ts
@@ -14,14 +14,14 @@ export default function getFutureMetadata(dateString: string, tokenIndex: string
       ...baseMetadata.attributes
     ],
     "description": `This photo will be taken on ${formattedDate}.`,
-    "image": "ar://ETzoxfKjsr5dZqWy-vlsQKV3iJnb2K7RICsQfHWASxM",
-    "image_url": "ar://ETzoxfKjsr5dZqWy-vlsQKV3iJnb2K7RICsQfHWASxM",
+    "image": "ar://yFPEI60kQ5BK8ANO3ggtzZS1Z50IhCI4pI2gVaX5qbk",
+    "image_url": "ar://yFPEI60kQ5BK8ANO3ggtzZS1Z50IhCI4pI2gVaX5qbk",
     "image_details": {
-      "size": 2856048,
+      "size": 3050814,
       "type": "image/jpeg",
       "width": 3000,
       "height": 2000,
-      "sha256": "c8707f747d32151a1e914f7759170bace6d82792afa84526bf328ed0ca2f65f0"
+      "sha256": "1158dc7988f4ec51040bef11de665f9471bdbeb8234eee89f83e77a242ff23d7"
     }
   }
 }
