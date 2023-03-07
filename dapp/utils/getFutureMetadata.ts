359c37ef6618b0e3eeacca31c1f75cefe8d99fb4
Philipp Adrian <hello@philippadrian.com>
2023-03-06 23:07:37 -0500

    Update getFutureMetadata.ts

--- dapp/utils/getFutureMetadata.ts
+++ dapp/utils/getFutureMetadata.ts
@@ -15,7 +15,7 @@ export default function getFutureMetadata(dateString: string, tokenIndex: string
     ],
     "description": `This photo will be taken on ${formattedDate}.
 
-[Cover Photo](btc://177220a04867e8dc79f0f9f66c0e7b65aebb257453ec9cfc9ea85dc4003e3b09i0)`,
+[Cover Photo Inscription](https://ordinals.com/inscription/177220a04867e8dc79f0f9f66c0e7b65aebb257453ec9cfc9ea85dc4003e3b09i0)`,
     "inscription": "btc://177220a04867e8dc79f0f9f66c0e7b65aebb257453ec9cfc9ea85dc4003e3b09i0",
     "image": "ar://yFPEI60kQ5BK8ANO3ggtzZS1Z50IhCI4pI2gVaX5qbk",
     "image_url": "ar://yFPEI60kQ5BK8ANO3ggtzZS1Z50IhCI4pI2gVaX5qbk",
